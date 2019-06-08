585df6c578a0abc9f772815a71ec9a47f2e92e57
Goran Gajic <fero.null@gmail.com>
2019-06-08 11:05:16 +0200

    listanje elemenata

--- App.js
+++ App.js
@@ -2,9 +2,22 @@ import React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const list = [{
+    text: 'zvati dalibora',
+    done: false,
+  }, {
+    text: 'doci na predavanje',
+    done: false,
+  }, {
+    text: 'nauciti react',
+    done: false,
+  }];
   return (
     <View style={styles.container}>
       <View style={styles.list}>
+        {list.map((item, index) => (<View key={index}>
+          <Text>{item.text}</Text>
+        </View>))}
       </View>
       <View style={styles.inputWrap}>
         <TextInput placeholder="New Task" style={styles.input} />
